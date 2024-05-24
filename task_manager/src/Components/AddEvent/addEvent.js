b13006e92266c7628c334f9364541a92efe6f5b3
Trevor Bisson <4trevorbisson@gmail.com>
2024-05-24 18:02:41 -0500

    addEvent component done(not Styled)

--- task_manager/src/Components/AddEvent/addEvent.js
+++ task_manager/src/Components/AddEvent/addEvent.js
@@ -1,40 +1,13 @@
-import react, {useState,useEffect} from "react";
+import { useState} from "react";
 import "./addEvent.scss"
 
 const AddEvent = () => {
 
     const [timeLength,changeTimeLength] = useState(0);
 
-    const [rgb,changeRGB] = useState({
-        red:0,
-        green:0,
-        blue:0
-    })
+    const [rgb,changeRGB] = useState("FFFFFF")
 
-    const inputTimeChangeHandler=(e)=>{
-        changeTimeLength((prevState)=>{
-            return{...prevState,[e.target.timeLength]:e.target.value}
-        })
-    }
-    
-    const inputRedColorChangeHandler=(e)=>{
-        changeRGB((prevState)=>{
-            return{...prevState,[e.target.red]:e.target.value}
-        })
-    }
-
-    const inputBlueColorChangeHandler=(e)=>{
-        changeRGB((prevState)=>{
-            return{...prevState,[e.target.blue]:e.target.value}
-        })
-    }
-
-    const inputGreenColorChangeHandler=(e)=>{
-        changeRGB((prevState)=>{
-            return{...prevState,[e.target.green]:e.target.value}
-        })
-    }
-
+    const h4style = { color: rgb }
     return (
         <div className="Add-Event">
             <form className="Form-Wrapper">
@@ -49,22 +22,16 @@ const AddEvent = () => {
                 </div>
                 <div className="section">
                     <h5>Proposed Time Length:</h5>
-                    <input type="range" min="0" max="240" step="5" value={timeLength} onChange={(e) => changeTimeLength(e.target.value)}/>
+                    <input type="range" min="0" max="240" step="5" value={timeLength} onChange={(e)=>changeTimeLength(e.target.value)}/>
                     <h5>{timeLength} minutes</h5>
                 </div>
                 <div className="section">
                     <h5>Description:</h5>
                     <input className="description"></input>
                 </div>
-                <div className="color-section">
-                    <h4>Event Color:</h4>
-                    <h5>Red:</h5>
-                    <input type="range" min="0" max="255" step="1" value={timeLength} onChange={inputRedColorChangeHandler}/>
-                    <h5>Blue:</h5>
-                    <input type="range" min="0" max="255" step="1" value={timeLength} onChange={inputBlueColorChangeHandler}/>
-                    <h5>Green:</h5>
-                    <input type="range" min="0" max="255" step="1" value={timeLength} onChange={inputGreenColorChangeHandler}/>
-
+                <div className="section">
+                    <h4 id="colorAnswer" style={h4style}>Event Color:</h4>
+                    <input type="color"  value={rgb} onChange={e => changeRGB(e.target.value)}/>
                 </div>
                 <button>Add Event</button>
             </form>
